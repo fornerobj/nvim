30b98e0822f7fb05173772affad89baaaf55bc30
fornerobj <fornerobj@gmail.com>
2024-04-24 15:13:58 -0500

    BLAH BLAH BLAH

--- lua/fornerobj/init.lua
+++ lua/fornerobj/init.lua
@@ -1,6 +1,8 @@
 require("fornerobj.lazy_init")
 require("fornerobj.remap")
 require("fornerobj.set")
+require("autoclose").setup()
+require("prettier").setup()
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
